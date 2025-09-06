07bf336c9c7e3a0825faf2f19695ece8891762d6
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-09-06 22:07:31 +0530

    Implement array rotation using reversal algorithm and update input parameters

--- RotateTheArrayToLeftByK.java
+++ RotateTheArrayToLeftByK.java
@@ -3,19 +3,40 @@ import java.util.List;
 import java.util.Scanner;
 
 public class RotateTheArrayToLeftByK {
+
+    static void reverse(int arr[] , int start , int end ){
+        while (start <= end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp ;
+             start++;
+             end--;
+        }
+    }
     static void leftRotate(int arr[] , int n , int d){
-        d= d%n;
-        List<Integer> temp = new ArrayList<>();
-        for(int i = 0 ; i<d ; i++){
-            temp.add(arr[i]);
-        }
-        for(int i = d ; i<n ; i++){
-            arr[i-d] = arr[i];
-        }
-        for(int i = n-d ; i<d ; i++){
-            arr[i] = temp.get(i-(n-d));
-        }
-        
+
+        //brute force
+
+         d= d%n;
+        // List<Integer> temp = new ArrayList<>();
+        // for(int i = 0 ; i<d ; i++){
+        //     temp.add(arr[i]);
+        // }
+        // for(int i = d ; i<n ; i++){
+        //     arr[i-d] = arr[i];
+        // }
+        // for(int i = n-d ; i<d ; i++){
+        //     arr[i] = temp.get(i-(n-d));
+        // }
+        reverse(arr,0,d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+
+
+        //optimal
+
+
+
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
