bc9000c42265e699c07b0761adf3c3c1263d9883
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-09-08 08:11:38 +0530

    Refactor union method to return ArrayList and optimize merging of two sorted arrays

--- UnionOfTheSortedArray.java
+++ UnionOfTheSortedArray.java
@@ -1,24 +1,64 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.TreeSet;
 import java.util.Set;
 
 public class UnionOfTheSortedArray {
 
-    public static int[] union(int arr1[],int arr2[]){
-        Set<Integer> st = new TreeSet<>();
-        for(int i = 0 ; i<arr1.length;i++){
-            st.add(arr1[i]);
-        }
-        for(int i = 0 ; i<arr2.length;i++){
-            st.add(arr2[i]);
-        }
-        int un[] = new int[st.size()];
-        int idx = 0 ; 
-        for(int num : st){
-            un[idx++] = num;
+    public static ArrayList<Integer> union(int arr1[],int arr2[]){
+
+        //Brute Force
+        // Set<Integer> st = new TreeSet<>();
+        // for(int i = 0 ; i<arr1.length;i++){
+        //     st.add(arr1[i]);
+        // }
+        // for(int i = 0 ; i<arr2.length;i++){
+        //     st.add(arr2[i]);
+        // }
+        // int un[] = new int[st.size()];
+        // int idx = 0 ; 
+        // for(int num : st){
+        //     un[idx++] = num;
+        // }
+
+        //Optimal 
+
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int i = 0 ;
+         int j = 0 ; 
+        ArrayList<Integer> unionArr = new ArrayList<>();
+        while(i<n1 && j < n2){
+            if(arr1[i] <= arr2[j]){
+               if( unionArr.size()==0 || unionArr.getLast() != arr1[i]){
+                unionArr.add(arr1[i]);
+               } 
+               i++;
+
+            }
+            else{
+                if( unionArr.size()==0 || unionArr.getLast() != arr2[i]){
+                unionArr.add(arr2[i]);
+               } 
+               j++;
+            }
         }
 
-        return un;
+
+        while(j<n2){
+             if( unionArr.size()==0 || unionArr.getLast() != arr2[i]){
+                unionArr.add(arr2[i]);
+               } 
+               j++;
+        }
+        while(i<n1){
+            if( unionArr.size()==0 || unionArr.getLast() != arr1[i]){
+                unionArr.add(arr1[i]);
+               } 
+               i++;
+        }
+
+        return unionArr;
 
     }
     
@@ -34,9 +74,9 @@ public class UnionOfTheSortedArray {
         for(int i = 0 ; i< n2 ; i++){
             arr2[i] = sc.nextInt();
         }
-        int unionSet[] = union(arr1,arr2);
-        for(int i = 0 ; i< unionSet.length;i++){
-            System.out.print(unionSet[i] + " ");
+        ArrayList<Integer> unionArr= union(arr1,arr2);
+        for(int i : unionArr){
+            System.out.print(i + " ");
         }
     }
 }
