694aec9c0af1f320eae1ce7911d0cbe61a3419ee
Vighnesh Shukla <vighneshshukla00@mgmail.com>
2025-10-11 18:44:12 +0530

    Update DSATemplate to correctly display decremented value and close scanner

--- DSATemplate.java
+++ DSATemplate.java
@@ -3,8 +3,9 @@ class DSATemplate{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        System.out.println(n--);
         System.out.println(n);
-
+        sc.close();
 
     }
 }
