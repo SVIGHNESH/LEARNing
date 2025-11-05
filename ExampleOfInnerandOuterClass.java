c3633877e073c1c91560721026c7528ce3d3107b
Vighnesh Shukla <vighneshshukla00@mgmail.com>
2025-11-05 20:34:35 +0530

    Refactor ExampleOfInnerandOuterClass to enhance inner class functionality and add overridden method in ArrayList

--- ExampleOfInnerandOuterClass.java
+++ ExampleOfInnerandOuterClass.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 class OuterClass {
   static int a = 10;
@@ -6,19 +8,36 @@ class OuterClass {
       System.out.println("This is the Outer Class Execute Method");
     }
 
-   class InnerClass {
+   static class InnerClass {
     public void execute(){
-      System.out.println("This is the inner Class Execute Method");
+      System.out.println("This is the inner Class Execute Method " + a);
     }
   }
 }
 
 class ExampleOfInnerandOuterClass{
   public static void main(String[] args) {
-    OuterClass obj = new OuterClass();
-    OuterClass.InnerClass ob= obj.new InnerClass();
-    ob.execute();
-    obj.execute(); 
+  
+    // OuterClass outer = new OuterClass();
+    OuterClass.InnerClass inner =new OuterClass.InnerClass();
+
+    //inner.execute(); 
+
+    ArrayList<Integer> ls = new ArrayList<>(){
+        @Override
+        public boolean add(Integer e )
+        {
+            System.out.println("This is the overriden Method Calling with the " + e );
+            super.add(e);
+            return true;
+        }
+
+    };
+    ls.add(345);
+    ls.add(2533);
+    System.out.println(ls);
+
+    
   }
   
     
